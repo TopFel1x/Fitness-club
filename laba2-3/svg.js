07de26ce508b4c6d41b77a0acbe17bbfcf027b17
TopFel1x <akopyan.felix02@gmail.com>
2023-06-28 16:13:28 +0300

    feat: add exercise 4 (use icon library and add animation)

---
+++ laba2-3/svg.js
@@ -0,0 +1,6 @@
+const burgerLink = document.querySelector(".burger-link")
+const burgerSymbol = document.getElementById("burger-menu")
+
+burgerLink.addEventListener("click", () => {
+  burgerSymbol.classList.add("active")
+})
