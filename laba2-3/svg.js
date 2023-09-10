13f38962d46e665907d73a85bed09acbc6cb9dc7
TopFel1x <akopyan.felix02@gmail.com>
2023-09-10 18:11:34 +0300

    feat: add exercise 6

--- laba2-3/svg.js
+++ laba2-3/svg.js
@@ -5,3 +5,19 @@ const burgerSymbol = document.getElementById("burger-menu")
 burgerLink.addEventListener("click", () => {
   burgerSymbol.classList.add("active")
 })
+
+// Exercise 6: Map
+const lastExit = document.querySelector("#last-exit")
+const span = document.querySelector(".map__span")
+const rect1 = document.querySelector(".map__rect1")
+const rect2 = document.querySelector(".map__rect2")
+const rect3 = document.querySelector(".map__rect3")
+const rect4 = document.querySelector(".map__rect4")
+
+lastExit.addEventListener("click", () => {
+  span.textContent = "Закрыто, идите отсюда!"
+})
+
+function numHouse(num) {
+  span.textContent = `Здание № ${num}`
+}
